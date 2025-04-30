8ba0c153b29a77dab2066e5e7d1cd47c55a9ee69
ElianUAM <165867067+ElianUAM@users.noreply.github.com>
2025-04-30 09:02:08 -0600

    Add files via upload

---
+++ ejercicio 3/ejercicio3.py
@@ -0,0 +1,9 @@
+#Diseñar un método “Convbinario” que reciba un entero como parámetro. 
+# La función, usando una pila, deberá mostrar el número en código binario.
+
+from modulobinario import convbinario
+
+
+print("---Bienvenido a Conversión Binaria---")
+num = int(input("Ingrese un número entero: "))
+convbinario(num)
